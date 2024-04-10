5d8a2ef75a85b811ecd844aec0e524d467d13819
choikanghyeok <163234723+21100258@users.noreply.github.com>
2024-04-10 22:03:22 +0900

    Add files via upload

---
+++ README.md/과제/a5/e3.java
@@ -0,0 +1,22 @@
+package a5;
+
+public class e3 {
+
+		public static void main(String[] args) {
+			for (Direction d : Direction.values()) {
+				System.out.print(d + " ");
+			}
+		}
+		enum Direction{
+			East("동쪽"),West("서쪽"),North("북쪽"),South("남쪽");
+			
+			private String s;
+			
+			private Direction(String s) {
+				this.s=s;
+			}
+			public String toString() {
+				return this.s;
+			}
+		}
+}
